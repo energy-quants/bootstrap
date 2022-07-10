0697705bf4f9e90cab2b3b3a18dd1b9a93692324
Dave Hirschfeld <dave.hirschfeld@gmail.com>
2022-07-10 10:31:00 +1000

    Add installer for `msodbc`

---
+++ linux/msodbc/README.md
@@ -0,0 +1,4 @@
+# Install the MSODBC Driver
+
+
+* https://docs.microsoft.com/en-us/sql/connect/odbc/linux-mac/installing-the-microsoft-odbc-driver-for-sql-server?view=sql-server-ver16#ubuntu18
---
+++ linux/msodbc/install.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# parse command line arguments
+set -euo pipefail
+script_dir="$(cd "$(dirname "$(readlink -e "${BASH_SOURCE[0]}")")" && pwd)"
+. "${script_dir}/argparse/install.sh"
+
+
+test "$(whoami)" == "root" || sudo -n whoami > /dev/null 2>&1 || {
+    echo "ERROR: You must be root to install mambaforge!"
+    exit 1
+}
+
+set -x
+
+apt-get update
+apt-get install -y curl gnupg lsb-release
+
+# Add Microsoft GPG key
+# FIXME: Key is stored in legacy trusted.gpg keyring
+curl -sL https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
+
+# Add MSSQL source
+curl -sL https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/prod.list > /etc/apt/sources.list.d/mssql-release.list
+
+apt-get update
+
+if [ "${_arg_list_versions}" == "on" ]; then
+    # List available versions and exit
+    apt list -a msodbcsql18
+    exit $?
+fi
+
+if [ "${_arg_version}" == "latest" ]; then
+    ACCEPT_EULA=Y apt-get install -y msodbcsql18
+else
+    ACCEPT_EULA=Y apt-get install -y msodbcsql18 "${_arg_version}"
+fi
